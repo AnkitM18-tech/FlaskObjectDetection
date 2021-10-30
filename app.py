c7f430f5a369b4d97eab9fd9fc166498acbeb26e
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-10-30 18:06:24 +0530

    detection and converting to numpy array

--- app.py
+++ app.py
@@ -21,3 +21,18 @@ PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 NUM_CLASSES = 90
 
 #Detection
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name="")
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map,max_num_classes=NUM_CLASSES,use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+#converting to numpy array
+def load_image_into_numpy_array(image):
+    (im_width,im_height)=image.size
+    return np.array(image.getdata()).reshape(im_height,im_width,3).astype(np.uint8)
